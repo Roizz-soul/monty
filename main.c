b478f393d2494605a6283fee86ae8a932d1f6e86
MergoldShey <soronnadishiphiah@gmail.com>
2022-10-05 16:29:46 +0000

    Header file

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point 
+ * @argv: argument vector
+ * @argc: argument count
+ *
+ * Return: Always 0
+ */
+
+int main (int argc, char *argv[])
+{
+
+
+
+
+
+
+
+}
