f95121c769ad48f4e41d7ef54a96981a1632ec91
MergoldShey <soronnadishiphiah@gmail.com>
2022-10-07 15:52:48 +0000

    Adjusted

--- monty.h
+++ monty.h
@@ -1,29 +1,23 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#define  _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <unistd.h>
 #include <string.h>
-#define INSTRUCTIONS \
-{\
-	{"push", _push},\
-	{"pall", _pall},\
-	{"pint", _pint},\
-	{"pop", _pop},\
-}
+#include <unistd.h>
+#include <ctype.h>
+
+/*--- Struct Definitions ---*/
+extern int push_arg;
 
 /**
-  * struct stack_s - doubly linked list representation of a stack (or queue)
-  * @n: integer
-  * @prev: points to the previous element of the stack (or queue)
-  * @next: points to the next element of the stack (or queue)
-  *
-  * Description: doubly linked list node structure
-  * for stack, queues, LIFO, FIFO
-  */
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct stack_s
 {
 	int n;
@@ -32,12 +26,11 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
+ * struct instruction_s - opcoode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
@@ -45,13 +38,52 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-char **global_command;
-void _pint(stack_t **stack, unsigned int line_number);
-void _push(stack_t **stack, unsigned int line_number);
-void _pall(stack_t **stack, unsigned int line_number);
-void _pop(stack_t **stack, unsigned int line_number);
-char **split_line(char *line);
-int _exec(stack_t *head, char **cmd_line, unsigned int line_number);
-stack_t *head;
+/**
+ * struct global_variable - opcoode and its function
+ * @file: the opcode
+ * @push_arg: function to handle the opcode
+ * @buffer: pointer to
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct global_variable
+{
+	FILE *file;
+	int push_arg;
+	char *buffer;
+} global_var;
 
-#endif /* MONTY_H */
+extern global_var var_global;
+
+void read_file(char *filename, stack_t **stack);
+char *parse_line(char *line, stack_t **stack, unsigned int line_number);
+typedef void (*instruct_func)(stack_t **stack, unsigned int line_number);
+instruct_func get_op_func(char *str);
+
+/*Fuxntions Monty*/
+void _pall(stack_t **stack, unsigned int line_number);
+void _push(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+void _rotl(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number);
+
+/*Functions string ascci */
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
+
+/*Functions math*/
+void _sub(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
+
+/*Utility functions*/
+void free_dlistint(stack_t *head);
+void dobfree(stack_t **stack);
+void dobfree2(stack_t **stack);
+int _isalpha(int c);
+#endif
